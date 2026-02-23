d8ed41fc6fe7fdfc38bcdcb785dcc4da1350de65
Sagiv Tirosh <sagivtirosh793@gmail.com>
2026-02-23 09:17:22 -0500

    First Part

--- Exploration2.cpp
+++ Exploration2.cpp
@@ -1,8 +1,11 @@
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHSD.h>
+#include <FEHMotor.h>
 
 // Declarations for analog optosensors
+FEHMotor left_motor(FEHMotor::Motor0,9.0);
+FEHMotor right_motor(FEHMotor::Motor1,9.0);
 AnalogInputPin right_opto(FEHIO::Pin2);
 AnalogInputPin middle_opto(FEHIO::Pin3);
 AnalogInputPin left_opto(FEHIO::Pin4);
@@ -42,6 +45,32 @@ void ERCMain()
     while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
     // Write the value returned by the optosensor to the screen
     // <ADD CODE HERE>
+    
+    // Middle Optosensor on straight line
+    LCD.Clear(BLACK);
+    if(middle_opto.Value() > 3.25 && middle_opto.Value() < 3.90){
+        LCD.WriteLine("Middle Optosensor is on the line");
+    }
+    else{
+        LCD.WriteLine("Middle Optosensor is off the line");
+    }
+    // Left Optosensor on curved line
+    LCD.Clear(BLACK);
+    if(left_opto.Value() > 3.25 && left_opto.Value() < 3.90){
+        LCD.WriteLine("Left Optosensor is on the line");
+    }
+    else{
+        LCD.WriteLine("Left Optosensor is off the line");
+    }
+    //Right photosensor
+    LCD.Clear(BLACK);
+    if(right_opto.Value() > 3.25 && right_opto.Value() < 3.90){
+        LCD.WriteLine("Right Optosensor is on the line");
+    }
+    else{
+        LCD.WriteLine("Right Optosensor is off the line");
+    }
+
 
     // Repeat process for remaining optosensors, and repeat all three for the curved line values
     // <ADD CODE HERE>
